ecf2d7f324916abf36d719cc21764204893040ad
Peter Pierre <binh@sunrise.vn>
2018-08-04 14:21:56 +0700

    first commit

---
+++ config/bundles.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Bean\Bundle\DevToolBundle\BeanDevToolBundle::class => ['dev' => true, 'test' => true],
+];
