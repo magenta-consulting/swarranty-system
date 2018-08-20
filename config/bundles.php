2845ca31b3ac3a30765a1d636ce607eece6abdd8
Peter Pierre <binh@sunrise.vn>
2018-08-20 23:04:48 +0700

    Exporter\Bridge\Symfony\Bundle\SonataExporterBundle::class => ['all' => true],

--- config/bundles.php
+++ config/bundles.php
@@ -12,6 +12,9 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
+    Exporter\Bridge\Symfony\Bundle\SonataExporterBundle::class => ['all' => true],
+	
+	
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
