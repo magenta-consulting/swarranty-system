5b835ea77f734c88eff94e78fb28d8dcd2e44006
binhle410 <binh@sunrise.vn>
2020-05-04 18:15:45 +0700

    date_default_timezone_set("Asia/Singapore");

--- src/Kernel.php
+++ src/Kernel.php
@@ -27,6 +27,7 @@ class Kernel extends BaseKernel
 
     public function registerBundles()
     {
+        date_default_timezone_set("Asia/Singapore");
         $contents = require $this->getProjectDir().'/config/bundles.php';
         foreach ($contents as $class => $envs) {
             if (isset($envs['all']) || isset($envs[$this->environment])) {
