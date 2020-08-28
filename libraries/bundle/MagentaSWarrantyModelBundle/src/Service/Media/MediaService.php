df6f0cb713d55700fdc22436264435df89e64ca0
binhle410 <binh@sunrise.vn>
2020-08-28 23:30:37 +0700

    value.receiptImageWarranty.organisation.system.adminToken

--- libraries/bundle/MagentaSWarrantyModelBundle/src/Service/Media/MediaService.php
+++ libraries/bundle/MagentaSWarrantyModelBundle/src/Service/Media/MediaService.php
@@ -2,27 +2,26 @@
 
 namespace Magenta\Bundle\SWarrantyModelBundle\Service\Media;
 
-use Magenta\Bundle\SWarrantyModelBundle\Entity\Media\Media;
 use Sonata\MediaBundle\Entity\MediaManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class MediaService extends MediaManager {
-	/** @var ContainerInterface $container */
-	private $container;
-	
-	public function generatePrivateUrl($mid, $format = 'admin') {
-		$mediaPrefix = $this->container->getParameter('MEDIA_API_PREFIX');
-		if($mediaPrefix === '/') {
-			$mediaPrefix = '';
-		}
-		
-		return $url = $this->container->getParameter('MEDIA_API_BASE_URL') . $mediaPrefix . sprintf('/media/%d/binaries/%s/view.json', $mid, $format);
-	}
-	
-	/**
-	 * @param ContainerInterface $container
-	 */
-	public function setContainer(ContainerInterface $container): void {
-		$this->container = $container;
-	}
+class MediaService extends MediaManager
+{
+    /** @var ContainerInterface */
+    private $container;
+
+    public function generatePrivateUrl($mid, $format = 'admin', $token = 'DEFAULT_ADMIN_TOKEN')
+    {
+        $mediaPrefix = $this->container->getParameter('MEDIA_API_PREFIX');
+        if ('/' === $mediaPrefix) {
+            $mediaPrefix = '';
+        }
+
+        return $url = $this->container->getParameter('MEDIA_API_BASE_URL').$mediaPrefix.sprintf('/media/%d/binaries/%s/view.json?token=%s', $mid, $format, $token);
+    }
+
+    public function setContainer(ContainerInterface $container): void
+    {
+        $this->container = $container;
+    }
 }
